4c0ce65b6e12b3ce89a799813d00fdf62c43290c
wangguohui <wangguohui@autohome.com.cn>
2018-11-26 09:56:39 +0800

    m测试页

---
+++ m/sw.js
@@ -0,0 +1,31 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+workbox.setConfig({
+  debug: true,
+  // modulePathPrefix: ""
+});
+workbox.skipWaiting();
+workbox.clientsClaim();
+// html 缓存
+workbox.routing.registerRoute(
+  new RegExp('/m/$'),
+  workbox.strategies.networkFirst({
+    cacheName: 'athm-html-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 10
+      })
+    ]
+  })
+);
+// css js
+workbox.routing.registerRoute(
+  new RegExp('https://x\.autoimg\.cn/.*\.(?:js|css)'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'athm-static-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 10
+      })
+    ]
+  })
+);
